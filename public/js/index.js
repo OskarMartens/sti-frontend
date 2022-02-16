33c964a9474a497e2bd10c8fa1982cc5a8fc7502
OskarMartens <martens.oskar@gmail.com>
2022-02-16 10:05:06 +0100

    test

---
+++ public/js/index.js
@@ -0,0 +1,39 @@
+var aDiv = document.getElementById('app');
+
+var buttonBox = document.createElement("div");
+    buttonBox.setAttribute("id", "Buttons");
+    buttonBox.style.width = "500px";
+    buttonBox.style.height ="800px";
+    Buttons.style.flex = "flex-direction; row";
+
+var addButton = document.createElement('button');
+    addButton.innerHTML = "Lägg till";
+    addButton.onclick = addElement;
+    addButton.style.margin = "8px";
+
+var removeButton = document.createElement('button');
+    removeButton.innerHTML = "Ta bort";
+    removeButton.onclick = removeElement;
+    removeButton.style.margin = "8px";
+
+var aTable = document.createElement('table');
+    aTable.setAttribute("id", "tavolo")
+    aTable.style.width = '200px';
+    aTable.style.height = '500px';
+    aTable.setAttribute('border', '100');
+
+aDiv.appendChild(aTable);
+/* aDiv.appendChild(buttonBox);
+buttonBox.appendChild(addButton);
+buttonBox.appendChild(removeButton); */
+
+function addElement(){
+    var aRow = document.createElement('tr');
+        var aText = document.createTextNode("New row");
+            aRow.appendChild(aText);
+        aTable.appendChild(aRow);
+}
+
+function removeElement(){
+    document.getElementsById("tavolo").deleteRow(0);
+}
