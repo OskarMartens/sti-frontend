4e0e1b8386ea8a53747a0630e0f11074aaecf880
OskarMartens <martens.oskar@gmail.com>
2022-02-17 08:35:20 +0100

    add files

---
+++ public/js/login.js
@@ -0,0 +1,18 @@
+var fullPage = document.getElementById('full-page')
+var mainHeader = document.getElementById('header')
+var subHeader = document.getElementById('sub-header')
+var login = document.getElementById('login')
+var footer = document.getElementById('footer')
+
+fullPage.appendChild(mainHeader)
+fullPage.appendChild(subHeader)
+fullPage.appendChild(login)
+fullPage.appendChild(footer)
+
+
+/* var mainDiv = document.getElementById("mainDiv") */
+
+
+
+
+
